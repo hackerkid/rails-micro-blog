7c5d834c9d31207954054aeb10a4d3a72e906291
Vishnu Ks <hackerkid@vishnuks.com>
2016-02-05 00:39:57 +0530

    guard and minitest added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,35 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  
+  def setup
+    @base_title = "MicroBloggger"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | MicroBloggger"
-  end
+    assert_select "title", "Home | #{@base_title}"
+    end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | MicroBloggger"
+    assert_select "title", "Help | #{@base_title}"
 
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About | MicroBloggger"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+   test "should get contact" do
+  	get :contact
+  	assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
